7b4bfdeccc064e302a9d71ef64aea308ef72a48a
Vlad Gregurco <gregurco.vlad@gmail.com>
2019-05-17 17:06:28 +0300

    Add slicing for home template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    Content
+    <v-layout row wrap>
+      <v-flex xs6>
+        <h1>Questions</h1>
+      </v-flex>
+
+      <v-flex xs6 class="text-xs-right">
+        <v-btn color="info">Ask Question</v-btn>
+      </v-flex>
+    </v-layout>
+
+    ...
   </div>
 </template>
 
